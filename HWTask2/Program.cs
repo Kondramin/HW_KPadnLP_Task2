8a18b7ad3d56ee107c75033e54eea2185d49834b
Артём Кондратьев <exitway12@gmail.com>
2020-05-25 18:07:31 +0300

    All project done.

---
+++ HWTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿using HWTask2ConsoleInterfaceLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ci = new ConsoleInterface();
+            ci.StartInterface();
+        }
+    }
+}
